67d7da01c25ab4495fffcbd4f7df64f504d43eea
Akos Bordas <akos.bordas@corballis.ie>
2019-03-14 15:11:33 +0100

    verify-intellij-formatter-consistency.sh

--- code-format/verify-intellij-formatter-consistency.sh
+++ code-format/verify-intellij-formatter-consistency.sh
@@ -14,6 +14,7 @@ cd /opt/intellij/*
 ./bin/format.sh -r $DEVELOPER_TOOLS_PATH/code-format/editor-verification-files -m "*.java|*.ts|*.js|*.html|*.css|*.json" -s $DEVELOPER_TOOLS_PATH/intellij-config/codestyles/Greencode.xml
 cd $DEVELOPER_TOOLS_PATH
 
+echo "Checking that the reference files are unchanged"
 HAS_CHANGE=$(git status | grep -c "modified:")
 if [ ${HAS_CHANGE} -gt 0 ]; then
 	echo "Probably some configuration properties have been changed in IntelliJ settings since the last verstion. The code format of the following files have been changed:"
