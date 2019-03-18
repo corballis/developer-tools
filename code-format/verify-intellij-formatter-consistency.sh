af08b1a8bc2cc67003c3ae43f19a9697200e184e
Akos Bordas <akos.bordas@corballis.ie>
2019-03-18 09:36:32 +0100

    verify-intellij-formatter-consistency.sh

--- code-format/verify-intellij-formatter-consistency.sh
+++ code-format/verify-intellij-formatter-consistency.sh
@@ -15,8 +15,7 @@ cd /opt/intellij/*
 cd $DEVELOPER_TOOLS_PATH
 
 echo "Checking that the reference files are unchanged"
-HAS_CHANGE=$(git status | grep "modified:" | wc -l)
-if [ ${HAS_CHANGE} -gt 0 ]; then
+if [[ $(git status | grep "modified:" | wc -l) -gt 0 ]]; then
 	echo "Probably some configuration properties have been changed in IntelliJ settings since the last verstion. The code format of the following files have been changed:"
 	git status
 	exit 1
