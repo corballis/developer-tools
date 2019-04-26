014310805cd35dc115415a35203fe740b4a14fa2
Akos Bordas <akos.bordas@corballis.ie>
2019-04-26 11:25:50 +0200

    sonarqube-analyzer.orb.yml added docker compose and analysis refined

--- quality-management/analyze.sh
+++ quality-management/analyze.sh
@@ -4,25 +4,29 @@
 
 cd $1
 
-SONAR_SERVER_URL=${SONAR_SERVER_URL:-http://localhost:9001}
+SONAR_SERVER_URL=$2
+SONAR_LOGIN=$3
 
 echo "Current directory:"
 pwd
 
-PROJECT_NAME=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' --non-recursive exec:exec 2>/dev/null)
-BRANCH_NAME=$(git branch | grep \* | cut -d ' ' -f2)
-PROJECT_KEY="$PROJECT_NAME:$BRANCH_NAME"
+if [ -z "${PROJECT_KEY}" ]; then
+	PROJECT_NAME=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' --non-recursive exec:exec 2>/dev/null)
+	BRANCH_NAME=$(git branch | grep \* | cut -d ' ' -f2)
+	PROJECT_KEY="$PROJECT_NAME:$BRANCH_NAME"
+fi
+
 echo "SonarQube will analyze this project with the following key: $PROJECT_KEY"
 
 echo "Waiting for SonarQube task to start (might take a while....)"
-TASK_URL=$(mvn -DskipTests sonar:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.projectName=$PROJECT_KEY | tee out | grep -Eo 'http.*/api/ce/task.*')
+TASK_URL=$(mvn -DskipTests sonar:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.projectName=$PROJECT_KEY -Dsonar.login=$SONAR_LOGIN | tee out | grep -Eo 'http.*/api/ce/task.*')
 
 echo "Task result will be available at $TASK_URL. Polling started..."
 
 CONNECT_RETRY=30
  
 counter=0
-while [ $(curl -s $TASK_URL 2>&1 | grep -q 'IN_PROGRESS'; echo $?) -eq 0 ]&&[ $counter -lt $CONNECT_RETRY ]; do
+while [ $(curl -s -u $SONAR_LOGIN: $TASK_URL 2>&1 | grep -q 'IN_PROGRESS'; echo $?) -eq 0 ]&&[ $counter -lt $CONNECT_RETRY ]; do
   sleep 5
   ((counter++))
   echo "waiting for SonarQube task to be ready ($counter/$CONNECT_RETRY)"
@@ -31,7 +35,7 @@ done
 echo "Task result: "
 curl -s $TASK_URL
 
-ANALYSIS_ID=$(curl -s $TASK_URL | sed -e 's/[{}]/''/g' | awk -v RS=',"' -F: '/^analysisId/ {print $2}' | grep -oP '"\K[^"]+')
+ANALYSIS_ID=$(curl -s -u $SONAR_LOGIN: $TASK_URL | sed -e 's/[{}]/''/g' | awk -v RS=',"' -F: '/^analysisId/ {print $2}' | grep -oP '"\K[^"]+')
 
 if [ -z "${ANALYSIS_ID}" ]; then
     echo "Something went wrong. ANALYSIS_ID is unset."
@@ -42,7 +46,7 @@ ANALYSIS_URL="$SONAR_SERVER_URL/api/qualitygates/project_status?analysisId=$ANAL
 
 echo "Verify Quality Gate results for analysis $ANALYSIS_ID ($ANALYSIS_URL)"
 
-if [[ $(curl -s $ANALYSIS_URL 2>&1 | grep "ERROR" | wc -l) -gt 0 ]]; then
+if [[ $(curl -s -u $SONAR_LOGIN: $ANALYSIS_URL 2>&1 | grep "ERROR" | wc -l) -gt 0 ]]; then
 	echo "Quality Gate has been broken:"
 	curl -s $ANALYSIS_URL
 	exit 1
