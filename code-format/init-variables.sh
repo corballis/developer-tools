29305643e578d6d8a75a3e400e08deb239e15b8b
laszlo.szabo <Laszlo_Szabo2@epam.com>
2020-02-20 10:49:11 +0100

    IntelliJ version has been upgraded to 2019.1.4

--- code-format/init-variables.sh
+++ code-format/init-variables.sh
@@ -3,7 +3,7 @@
 #set -e
 
 # Here you can initialize the defaults to the environment variables which are required for IntelliJ code formatting. 
-INTELLIJ_VERSION=${INTELLIJ_VERSION:-2019.1.3}
+INTELLIJ_VERSION=${INTELLIJ_VERSION:-2019.1.4}
 INTELLIJ_CONFIG_FOLDER=${INTELLIJ_CONFIG_FOLDER:-/home/circleci/.IntelliJIdea2019.1/config}
 
 echo "Working with the following environment: "
