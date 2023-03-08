b40a98f6a93cbfe0852347fb247f09678c3cd2f7
Gabor Szmetanko <gabor.szmetanko@corballis.ie>
2023-03-08 13:34:36 +0100

    Update to IntelliJ 2022.3.2

--- code-format/init-variables.sh
+++ code-format/init-variables.sh
@@ -4,8 +4,8 @@
 
 # Here you can initialize the defaults to the environment variables which are required for IntelliJ code formatting. 
 WORKING_DIRECTORY=${WORKING_DIRECTORY:-/home/circleci}
-INTELLIJ_VERSION=${INTELLIJ_VERSION:-2021.3.2}
-INTELLIJ_CONFIG_FOLDER=${INTELLIJ_CONFIG_FOLDER:-/home/circleci/.config/JetBrains/IntelliJIdea2021.3}
+INTELLIJ_VERSION=${INTELLIJ_VERSION:-2022.3.2}
+INTELLIJ_CONFIG_FOLDER=${INTELLIJ_CONFIG_FOLDER:-/home/circleci/.config/JetBrains/IntelliJIdea2022.3}
 
 echo "Working with the following environment: "
 echo "WORKING_DIRECTORY = $WORKING_DIRECTORY"
